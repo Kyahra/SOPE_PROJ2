92e20a442af5c90b931f54cf3b58b47b7996397f
kyahra <up201504570@I00CS0302.fe.up.pt>
2017-05-12 18:47:26 +0100

    utils created

--- gerador.c
+++ gerador.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
+#include "utils.h"
 
 #define BUFFER_SIZE 512
 #define ID_MAX_SIZE 10
@@ -17,6 +18,7 @@ int * denied_requests;
 int fRequestsNum, mRequestsNum;
 int fDeniedNum, mDeniedNum;
 int fDiscardedRequests, fDiscardedRequests;
+struct timespec init_time;
 
 struct request{
   double duration;
@@ -24,80 +26,13 @@ struct request{
   int serial_number;
 };
 
-int readline(int fd, char *str);
+
 void sendRequest(int fd, int max_requests, int max_duration);
-void writeDescriptor(char *type, char * id, char * gender, char * dur);
-
-void *denied_request_handler(void * null)
-{
-
-  int fd;
-  mkfifo("/tmp/rejeitados",0660);
-  fd = open("/tmp/rejeitados", O_RDONLY);
-
-  if(fd){
-    perror("/tmp/rejeitados");
-    exit(2);
-  }
-
-  unsigned char buffer[BUFFER_SIZE];
-
-  while(readline(fd,buffer)){
-    char delimiter[] = " ";
-    char * serial_number_str, *gender_str, *duration_str;
-    int bufferLength = strlen(buffer);
-    char *bufferCopy = (char*) calloc(bufferLength + 1, sizeof(char));//copiar string porque strtok altera a mesma
-    strncpy(bufferCopy, buffer, bufferLength);
-    int serial_number, duration;
-
-    printf("%d\n", 1);
-
-    /*
-    * extrair palavras separadamente da frase
-    */
-    serial_number_str = strtok (bufferCopy, delimiter);
-    serial_number = atoi(serial_number_str);
-    gender_str = strtok (NULL, delimiter);
-    duration_str = strtok (NULL, delimiter);
-    duration = atoi (duration_str);
-
-    writeDescriptor("REJEITADO", serial_number_str, gender_str, duration_str);
-    if(strcmp(gender_str, "F") == 0) {
-      fDeniedNum++;
-    } else {
-      mDeniedNum++;
-    }
-  printf("%d\n", 2);
-    denied_requests[serial_number]++;
-
-    if(denied_requests[serial_number] < 3) {
-      //escrever no FIFO /tmp/entrada
-      int fd_resend = open("/tmp/entrada", O_WRONLY | O_CREAT | O_APPEND, 0644);
-      if(fd_resend == -1) {
-        perror("/tmp/entrada");//erro a abrir
-        close(fd_resend);
-        exit(3); //TODO: mandar sinal para acabar com o programa
-      }
-      if(write(fd_resend, buffer, bufferLength) != bufferLength) {
-        perror("/tmp/entrada");//erro a escrever
-        close(fd_resend);
-        exit(4);
-      }
-      close(fd_resend);
-  printf("%d\n", 3);
-      writeDescriptor("PEDIDO", serial_number_str, gender_str, duration_str);
-    } else if(denied_requests[serial_number] == 3) {
-      writeDescriptor("DESCARTADO", serial_number_str, gender_str, duration_str);
-    }
-  printf("%d\n", 4);
-  }
-
- return NULL;
-
-}
-
+void *denied_request_handler(void * null);
 
 int main(int argc, char* argv[]){
+
+  clock_gettime(CLOCK_MONOTONIC_RAW, &init_time);
 
   if(argc != 3) {
     printf("usage: gerador <n. pedidos> <max. utilização>\n");
@@ -109,7 +44,7 @@ int main(int argc, char* argv[]){
 
   denied_requests = (int *) malloc(sizeof(int)* max_requests);
 
-  int fd = open("/tmp/entrada", O_WRONLY | O_NONBLOCK | O_APPEND);
+  int fd = open("/tmp/entrada", O_WRONLY  | O_APPEND);
 
   if(fd< 0){
     perror("/tmp/entrada");
@@ -121,7 +56,7 @@ int main(int argc, char* argv[]){
     pthread_t handler_tid;
     pthread_t generator_tid = pthread_self();
 
-    rc = pthread_create(&handler_tid, NULL, denied_request_handler,&generator_tid);
+    //rc = pthread_create(&handler_tid, NULL, denied_request_handler,&generator_tid);
 
   sendRequest(fd, max_requests, max_duration);
 
@@ -170,37 +105,77 @@ void sendRequest(int fd, int max_requests, int max_duration){
     sprintf(serial_number, "%d", i);
 
       write(fd, "\n", 1);
-      writeDescriptor("PEDIDO", serial_number, gend, duration);
+      writeDescriptor("PEDIDO", i, gend, rd, init_time);
       sleep(2);
     }
 
 }
 
-void writeDescriptor(char *type, char * id, char * gender, char * dur){
+/*
+void *denied_request_handler(void * null)
+{
 
-   FILE * fp;
-   char file_name[100];
+  int fd;
+  mkfifo("/tmp/rejeitados",0660);
+  fd = open("/tmp/rejeitados", O_RDONLY);
 
+  if(fd){
+    perror("/tmp/rejeitados");
+    exit(2);
+  }
 
-   time_t rawtime;
-   struct tm * timeinfo;
-   time(& rawtime);
-   timeinfo = localtime( &rawtime);
+  unsigned char buffer[BUFFER_SIZE];
 
-   sprintf(file_name, "/tmp/ger.%d", (int) getpid());
+  while(readLine(fd,buffer)){
+    char delimiter[] = " ";
+    char * serial_number_str, *gender_str, *duration_str;
+    int bufferLength = strlen(buffer);
+    char *bufferCopy = (char*) calloc(bufferLength + 1, sizeof(char));//copiar string porque strtok altera a mesma
+    strncpy(bufferCopy, buffer, bufferLength);
+    int serial_number, duration;
 
-  fp = fopen (file_name, "a+");
-  fprintf(fp, "%d:%d:%d - %d - %s: %s - %s - %s\n",timeinfo->tm_hour,timeinfo->tm_min,timeinfo->tm_sec,
-  getpid(),id,gender,dur,type);
+    printf("%d\n", 1);
 
-  fclose(fp);
+    /*
+    * extrair palavras separadamente da frase
 
-}
+    serial_number_str = strtok (bufferCopy, delimiter);
+    serial_number = atoi(serial_number_str);
+    gender_str = strtok (NULL, delimiter);
+    duration_str = strtok (NULL, delimiter);
+    duration = atoi (duration_str);
 
-int readline(int fd, char *str){
-  int n;
-  do{
-      n = read(fd,str,1);
-    }while (n>0 && *str++ != '\0');
-    return (n>0);
-}
+    writeDescriptor("REJEITADO", serial_number_str, gender_str, duration_str, init_time);
+    if(strcmp(gender_str, "F") == 0) {
+      fDeniedNum++;
+    } else {
+      mDeniedNum++;
+    }
+  printf("%d\n", 2);
+    denied_requests[serial_number]++;
+
+    if(denied_requests[serial_number] < 3) {
+      //escrever no FIFO /tmp/entrada
+      int fd_resend = open("/tmp/entrada", O_WRONLY | O_CREAT | O_APPEND, 0644);
+      if(fd_resend == -1) {
+        perror("/tmp/entrada");//erro a abrir
+        close(fd_resend);
+        exit(3); //TODO: mandar sinal para acabar com o programa
+      }
+      if(write(fd_resend, buffer, bufferLength) != bufferLength) {
+        perror("/tmp/entrada");//erro a escrever
+        close(fd_resend);
+        exit(4);
+      }
+      close(fd_resend);
+  printf("%d\n", 3);
+      writeDescriptor("PEDIDO", serial_number_str, gender_str, duration_str, init_time);
+    } else if(denied_requests[serial_number] == 3) {
+      writeDescriptor("DESCARTADO", serial_number_str, gender_str, duration_str, init_time);
+    }
+  printf("%d\n", 4);
+  }
+
+ return NULL;
+
+}*/
